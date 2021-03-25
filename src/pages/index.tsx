4e23462823d4ad3124faa4a66bbf66f2f6078d1f
TianenPang <tianenpang@gmail.com>
2021-03-26 05:28:16 +0800

    update layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Home from './Home';
+import { Layout } from 'antd';
 import NavBar from '../components/NavBar';
+import Home from './Home';
 
 const Page: React.FC = (): JSX.Element => {
 
   return (
     <>
       <NavBar/>
-      <Home/>
+      <Layout.Content style={ { margin: '50px auto', padding: '50px 24px', border: '1px solid rgb(48, 48, 48)', maxWidth: '1440px' } }>
+        <Home/>
+      </Layout.Content>
     </>
   );
 };
