1a4336e3a713be5ffa5a0e62186a570491b67619
TianenPang <tianenpang@gmail.com>
2021-03-25 20:31:21 +0800

    initial home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Home: React.FC = (): JSX.Element => {
+
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
