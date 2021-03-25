72be38b7085e0c54d317370db49860155b10b0bc
TianenPang <tianenpang@gmail.com>
2021-03-25 20:33:51 +0800

    import page index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Page from './pages';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Page/>
   </React.StrictMode>,
   document.querySelector('#root')
 );
