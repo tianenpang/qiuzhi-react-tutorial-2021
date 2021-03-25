cb48712186b1e5891004eecd9cfadefdabe0f846
TianenPang <tianenpang@gmail.com>
2021-03-26 07:07:15 +0800

    responsively content layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Col, Layout, Row } from 'antd';
 import NavBar from '../components/NavBar';
 import Home from './Home';
 
@@ -8,9 +8,19 @@ const Page: React.FC = (): JSX.Element => {
   return (
     <>
       <NavBar/>
-      <Layout.Content style={ { margin: '50px auto', padding: '50px 24px', border: '1px solid rgb(48, 48, 48)', maxWidth: '1440px' } }>
-        <Home/>
-      </Layout.Content>
+      <Row align='middle'>
+        <Col
+          xs={ { span: 22, offset: 1 } }
+          sm={ { span: 20, offset: 2 } }
+          md={ { span: 18, offset: 3 } }
+          lg={ { span: 18, offset: 3 } }
+          xl={ { span: 16, offset: 4 } }
+        >
+          <Layout.Content style={ { margin: '50px auto', padding: '50px 24px', border: '1px dashed rgb(48, 48, 48)' } }>
+            <Home/>
+          </Layout.Content>
+        </Col>
+      </Row>
     </>
   );
 };
