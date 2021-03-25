f03b97523b8a7fa5d7fe99620f8e1bb018f6fdc1
TianenPang <tianenpang@gmail.com>
2021-03-26 05:29:45 +0800

    beautify navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Layout, Row, Col, Menu } from 'antd';
+import GithubCorner from 'react-github-corner';
 import './index.css';
 
 const NavBar: React.FC = (): JSX.Element => {
 
   return (
-    <div className='navbar'>
-      <h1>The Dojo Blog</h1>
-      <div className='links'>
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
-      </div>
-    </div>
+    <Layout.Header className='header'>
+      <Row className='header-row' align='middle'>
+        <Col span={ 4 }>
+          <span className='brand-title'>Brand Title</span>
+        </Col>
+        <Col span={ 20 }>
+          <Menu className='header-menu' mode="horizontal">
+            <Menu.Item className='header-menu-item'>Home</Menu.Item>
+            <Menu.Item className='header-menu-item'>New Blog</Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
+      <GithubCorner
+        size='64px'
+        target='_blank'
+        bannerColor='#000000'
+        ariaLabel='Fork me on GitHub'
+        href="https://github.com/TianenPang/qiuzhi-react-tutorial-2021"
+      />
+    </Layout.Header>
   );
 };
 
