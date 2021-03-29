0e61f2bdc05bc599d8953a356f471e319584cdee
TianenPang <tianenpang@gmail.com>
2021-03-29 22:18:21 +0800

    click event with useState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'antd';
 import './index.css';
 
 const Home: React.FC = (): JSX.Element => {
 
+  const [ name, setName ] = useState<string>('...');
+
+  const buttonClickHandler: Function = (): void => {
+    setName('Hello');
+  };
+
   return (
     <div className="home">
       <h1>Home Page</h1>
+      <h3>name: { name }</h3>
+      <Button type='dashed' onClick={ () => buttonClickHandler() }>Change Name</Button>
     </div>
   );
 };
