41207f1e3d982511d83b334e7733d23ffe4e9efa
TianenPang <tianenpang@gmail.com>
2021-04-02 16:08:07 +0800

    fetch data from mock api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,50 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { message } from 'antd';
 import BlogList from '../../components/BlogList';
 import './index.css';
 
+
 const Home: React.FC = (): JSX.Element => {
 
-  const [ blogs, setBlogs ] = useState<IBlog[]>([
-    {
-      id: 1,
-      author: 'admin',
-      title: 'vivamus arcu',
-      body: 'egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu'
-    }, {
-      id: 2,
-      author: 'admin',
-      title: 'nisl vel',
-      body: 'id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada'
-    }, {
-      id: 3,
-      author: 'admin',
-      title: 'posuere urna',
-      body: 'sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi'
-    }, {
-      id: 4,
-      author: 'admin',
-      title: 'aliquam sem',
-      body: 'purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst'
-    }, {
-      id: 5,
-      author: 'admin',
-      title: 'in cursus',
-      body: 'ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat'
-    }, {
-      id: 6,
-      author: 'admin',
-      title: 'viverra nibh',
-      body: 'et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum'
-    }
-  ]);
+  const [ blogs, setBlogs ] = useState<IBlog[]>([]);
+  const [ loading, setLoading ] = useState<boolean>(true);
 
   const deleteBlogHandler: Function = (id: number): void => {
     setBlogs([ ...blogs.filter(blog => blog.id !== id) ]);
   };
 
+  useEffect(() => {
+    fetch('https://run.mocky.io/v3/6802ea8b-eba8-4e4d-bac1-ff468802981a')
+      .then(response => response.json())
+      .then(data => {
+        setBlogs(data.blogs);
+        setLoading(false);
+      }).catch(() => message.warning('get some error on fetch data.'));
+  }, []);
+
   return (
     <div className='home'>
-      <BlogList title='All Blogs' data={ blogs } onDelete={ id => deleteBlogHandler(id) }/>
+      <BlogList title='All Blogs' data={ blogs } loading={ loading } onDelete={ id => deleteBlogHandler(id) }/>
     </div>
   );
 };
