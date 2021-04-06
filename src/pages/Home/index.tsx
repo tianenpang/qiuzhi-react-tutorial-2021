82c65848e83eb50741b6a107c04659403f07973b
TianenPang <tianenpang@gmail.com>
2021-04-06 15:30:10 +0800

    change fetch to useFetch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { notification } from 'antd';
+import { useFetch } from '../../hooks';
 import BlogList from '../../components/BlogList';
 import './index.css';
 
 
 const Home: React.FC = (): JSX.Element => {
 
-  const [ blogs, setBlogs ] = useState<IBlog[]>([]);
-  const [ loading, setLoading ] = useState<boolean>(true);
+  const blogs_api = 'https://run.mocky.io/v3/6802ea8b-eba8-4e4d-bac1-ff468802981a';
 
-  const deleteBlogHandler: Function = (id: number): void => {
-    setBlogs([ ...blogs.filter(blog => blog.id !== id) ]);
-  };
-
-  useEffect(() => {
-    fetch('https://run.mocky.io/v3/6802ea8b-eba8-4e4d-bac1-ff468802981a')
-      .then(response => {
-        if (!response.ok) throw Error('Could not fetch the data from the resource');
-        return response.json();
-      })
-      .then(data => {
-        setBlogs(data.blogs);
-        setLoading(false);
-      }).catch(error => onFetchErrorHandler(error.message));
-  }, []);
+  const [ data, loading ] = useFetch<{ blogs: IBlog[] }>(blogs_api, {
+    onError: message => onFetchErrorHandler(message)
+  });
 
   const onFetchErrorHandler = (description: string): void => {
     notification.warning({
@@ -35,7 +23,11 @@ const Home: React.FC = (): JSX.Element => {
 
   return (
     <div className='home'>
-      <BlogList title='All Blogs' data={ blogs } loading={ loading } onDelete={ id => deleteBlogHandler(id) }/>
+      <BlogList
+        title='All Blogs'
+        loading={ loading }
+        data={ data?.blogs }
+      />
     </div>
   );
 };
