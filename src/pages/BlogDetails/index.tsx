31db59c66637cf068049bc17e46d8899c1ad4f35
TianenPang <tianenpang@gmail.com>
2021-04-14 15:31:16 +0800

    add delete action

--- src/pages/BlogDetails/index.tsx
+++ src/pages/BlogDetails/index.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { DeleteOutlined } from '@ant-design/icons';
 import { useHistory, useParams } from 'react-router-dom';
-import { Card, Col, Divider, Row, Space, Typography } from 'antd';
+import { Card, Popconfirm, Col, Divider, Row, Space, Typography, Button, message } from 'antd';
 import { useFetch } from '../../hooks';
 import { fetchErrorHandler } from '../../utils';
 
-const { Title, Paragraph, Text, Link } = Typography;
+const { Paragraph, Text, Link } = Typography;
 
 const BlogDetails: React.FC = (): JSX.Element => {
 
   const history = useHistory();
   const { id } = useParams<IArticleParam>();
+  const [ deleting, setDeleting ] = useState<boolean>(false);
   const article_api = `https://run.mocky.io/v3/d8150bb2-6fdb-4ffa-ae9b-fe858aa5759e?id=${ id }`;
 
   const [ data, loading, error ] = useFetch<IBlog>(article_api);
@@ -18,14 +20,42 @@ const BlogDetails: React.FC = (): JSX.Element => {
     error && fetchErrorHandler(error);
   }, [ error ]);
 
+  const onArticleDeleteHandler = (id: number) => {
+    setDeleting(true);
+    fetch(`https://run.mocky.io/v3/53ad2462-f389-492a-b6ec-cca3fcac1e02?id=${ id }`)
+      .then(response => {
+        if (!response.ok) throw Error('Could not fetch to delete from the resource');
+        return response.json();
+      }).then(data => {
+      setDeleting(false);
+      if (data.success) {
+        history.push('/');
+        return message.success(`The article with id ${ id } is deleted successfully`);
+      }
+    }).catch(error => {
+      setDeleting(false);
+      fetchErrorHandler(error.message);
+    });
+  };
+
+  const cardExtra = (
+    <Popconfirm
+      okText='Delete'
+      cancelText='Cancel'
+      placement='bottomRight'
+      title='Please confirm to delete'
+      onConfirm={ () => data && onArticleDeleteHandler(data.id) }
+    >
+      <Button icon={ <DeleteOutlined/> } loading={ deleting }>
+        Delete
+      </Button>
+    </Popconfirm>
+  );
+
   const ArticleTypography = () => (
-    <Card loading={ loading }>
+    <Card title={ data?.title } loading={ loading } extra={ cardExtra }>
       <Typography>
-        <Title level={ 2 }>{ data?.title }</Title>
-        <Space direction='vertical' size='small'>
-          <Text>ID - { id }</Text>
-          <Text>Author - { data?.author }</Text>
-        </Space>
+        <Text>{ id } - { data?.author }</Text>
         <Divider/>
         <Paragraph>{ data?.body }</Paragraph>
         <Text>Just Mock Data!!!</Text>
